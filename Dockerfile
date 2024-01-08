15b61cb1118b6ac6398bb994bebb699448352b37
oprisorandreea <andreea.oprisor@student.upt.ro>
2024-01-08 15:30:48 +0200

    am modificat codul

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM gcc:latest
-WORKDIR /usr/src/myapp
-COPY myapp.c .
-RUN gcc -o myapp myapp.c
-CMD ["./myapp"]
--- myapp.c
+++ myapp.c
@@ -2,11 +2,11 @@
 
 #define N 8
 
-int moveX[8] = {2, 1, -1, -2, -2, -1, 1, 2};
-int moveY[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+int moveX[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+int moveY[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
 
 int isSafe(int x, int y, int sol[N][N]) {
-    return (x >= 0 && y >= 0 && x < N && y < N && sol[x][y] == -1);
+    return (x >= 0 && y >= 0 && x < N&& y < N&& sol[x][y] == -1);
 }
 
 void printSolution(int sol[N][N]) {
@@ -30,7 +30,8 @@ int solveKTUtil(int x, int y, int movei, int sol[N][N]) {
             sol[next_x][next_y] = movei;
             if (solveKTUtil(next_x, next_y, movei + 1, sol)) {
                 return 1;
-            } else {
+            }
+            else {
                 sol[next_x][next_y] = -1;
             }
         }
@@ -51,11 +52,12 @@ int solveKT(int x, int y) {
     sol[x][y] = 0; // Poziția de pornire a calului.
 
     if (solveKTUtil(x, y, 1, sol)) {
-        printf("Solutia este:\n");
+        printf("Solutia pentru coordonatele (%d, %d) este:\n", x, y);
         printSolution(sol);
         return 1;
-    } else {
-        printf("Nu există soluție.\n");
+    }
+    else {
+        printf("Nu există soluție pentru coordonatele (%d, %d).\n", x, y);
         return 0;
     }
 }
@@ -63,15 +65,23 @@ int solveKT(int x, int y) {
 int main() {
     int x, y;
 
-    printf("Introduceti coordonatele initiale ale calului (x și y): ");
-    scanf("%d %d", &x, &y);
+    while (1) {
+        printf("Introduceti coordonatele initiale ale calului (x și y) sau -1 pentru a iesi: ");
+        scanf("%d", &x);
 
-    if (x < 0 || x >= N || y < 0 || y >= N) {
-        printf("Coordonatele introduse nu sunt valide.\n");
-        return 1;
+        if (x == -1) {
+            break; // Ieșirea din buclă dacă utilizatorul introduce -1.
+        }
+
+        scanf("%d", &y);
+
+        if (x < 0 || x >= N || y < 0 || y >= N) {
+            printf("Coordonatele introduse nu sunt valide.\n");
+            continue; // Continuarea buclei pentru a aștepta noi coordonate.
+        }
+
+        solveKT(x, y);
     }
-
-    solveKT(x, y);
 
     return 0;
 }
